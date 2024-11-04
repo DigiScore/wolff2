df18636b95fa7f1802a3b02fcccd1812636d2dca
craigvear <craigvear123@gmail.com>
2024-11-04 14:35:44 +0000

    mod clock to local time

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # [HARDWARE]
-xarm_connected = False
-eda_live = False
+xarm_connected = True
+eda_live = True
 
 # [PLAY PARAMS]
 silence_listener = False
-duration_of_piece = 2000000  # in sec
+duration_of_piece = 240  # in sec
 speed = 5  # dynamic tempo of the all processes: 1 = slow, 10 = fast
 temperature = 0
 
@@ -27,7 +27,7 @@ mic_in_logging = False
 # [BITALINO]
 baudrate = 10
 channels = [0]
-mac_address = "98:D3:B1:FD:3D:1F"  # '/dev/cu.BITalino-3F-AE' (Linux)
+mac_address = "00:21:08:35:17:C0" #"98:D3:B1:FD:3D:1F"  # '/dev/cu.BITalino-3F-AE' (Linux)
 
 # [STREAMING]
 stream_list = ['rnd_poetry',

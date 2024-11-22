2a40956d476cc034d0d906006d0d9d17b392f44d
craigv <vear2ev2.co.uk>
2024-11-22 10:06:18 +0000

    added new RAMI ECG plotting from colab

--- modules/bitalino_visualiser.py
+++ modules/bitalino_visualiser.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
-# from opensignalsreader import OpenSignalsReader
 import neurokit2 as nk
+from neurokit2 import NeuroKitWarning
+from neurokit2.signal.signal_rate import _signal_rate_plot
+from neurokit2.ecg.ecg_peaks import _ecg_peaks_plot
 from sklearn.preprocessing import MinMaxScaler
+from warnings import warn
+import matplotlib.gridspec
+import numpy as np
+
 import config
+
 
 # example of data
 # id d1 d2 d3 d4 x y z eda hr bth
@@ -54,7 +61,7 @@ class BitalinoVisualiser:
         nk.eda_plot(eda_signals, eda_info)
 
         ecg_signals, ecg_info = nk.ecg_process(ecg, sampling_rate=self.sampling_rate)
-        nk.ecg_plot(ecg_signals, ecg_info)
+        self.rami_ecg_plot(ecg_signals, ecg_info)
 
         rsp_signals, rsp_info = nk.rsp_process(rsp, sampling_rate=self.sampling_rate)
         nk.rsp_plot(rsp_signals, rsp_info)
@@ -84,6 +91,109 @@ class BitalinoVisualiser:
         plt.show()
         plt.savefig(self.figures_path)
 
+    def rami_ecg_plot(self, ecg_signals, info=None):
+        """**Visualize ECG data**
+
+        Plot ECG signals and R-peaks.
+
+        Parameters
+        ----------
+        ecg_signals : DataFrame
+            DataFrame obtained from ``ecg_process()``.
+        info : dict
+            The information Dict returned by ``ecg_process()``. Defaults to ``None``.
+
+        See Also
+        --------
+        ecg_process
+
+        Returns
+        -------
+        Though the function returns nothing, the figure can be retrieved and saved as follows:
+
+        .. code-block:: python
+
+          # To be run after ecg_plot()
+          fig = plt.gcf()
+          fig.set_size_inches(10, 12, forward=True)
+          fig.savefig("myfig.png")
+
+        Examples
+        --------
+        .. ipython:: python
+
+          import neurokit2 as nk
+
+          # Simulate data
+          ecg = nk.ecg_simulate(duration=15, sampling_rate=1000, heart_rate=80)
+
+          # Process signal
+          signals, info = nk.ecg_process(ecg, sampling_rate=1000)
+
+          # Plot
+          @savefig p_ecg_plot.png scale=100%
+          nk.ecg_plot(signals, info)
+          @suppress
+          plt.close()
+
+        """
+        # Sanity-check input.
+        if not isinstance(ecg_signals, pd.DataFrame):
+            raise ValueError(
+                "NeuroKit error: ecg_plot(): The `ecg_signals` argument must be the "
+                "DataFrame returned by `ecg_process()`."
+            )
+
+        # Extract R-peaks.
+        if info is None:
+            warn(
+                "'info' dict not provided. Some information might be missing."
+                + " Sampling rate will be set to 1000 Hz.",
+                category=NeuroKitWarning,
+            )
+            info = {"sampling_rate": 1000}
+
+        # Extract R-peaks (take those from df as it might have been cropped)
+        if "ECG_R_Peaks" in ecg_signals.columns:
+            info["ECG_R_Peaks"] = np.where(ecg_signals["ECG_R_Peaks"] == 1)[0]
+
+        # Prepare figure and set axes.
+        gs = matplotlib.gridspec.GridSpec(2, 2, width_ratios=[2 / 3, 1 / 3])
+
+        fig = plt.figure(constrained_layout=False)
+        fig.suptitle("Electrocardiogram (ECG)", fontweight="bold")
+
+        ax0 = fig.add_subplot(gs[0, :])
+        ax1 = fig.add_subplot(gs[1, :], sharex=ax0)
+        # ax2 = fig.add_subplot(gs[:, -1])
+
+        # Plot signals
+        phase = None
+        if "ECG_Phase_Ventricular" in ecg_signals.columns:
+            phase = ecg_signals["ECG_Phase_Ventricular"].values
+
+        ax0 = _ecg_peaks_plot(
+            ecg_signals["ECG_Clean"].values,
+            info=info,
+            sampling_rate=info["sampling_rate"],
+            raw=ecg_signals["ECG_Raw"].values,
+            quality=ecg_signals["ECG_Quality"].values,
+            phase=phase,
+            ax=ax0,
+        )
+
+        # Plot Heart Rate
+        ax1 = _signal_rate_plot(
+            ecg_signals["ECG_Rate"].values,
+            info["ECG_R_Peaks"],
+            sampling_rate=info["sampling_rate"],
+            title="Heart Rate",
+            ytitle="Beats per minute (bpm)",
+            color="#FF5722",
+            color_mean="#FF9800",
+            color_points="#FFC107",
+            ax=ax1,
+        )
 
 if __name__ == "__main__":
     test = BitalinoVisualiser('../data/1732268552.4228742/bitalino/Bitalino_2024_11_22_0942.json',
---
+++ tests/ignore.py
@@ -0,0 +1,31 @@
+import mido
+
+from mido import MidiFile, Message, MidiTrack
+
+midsource = MidiFile("cfgc_124_reaper_type1_multitrack_960ppqn_embedTEMPO.mid")
+
+print("MIDI Type:", midsource.type)
+
+midfinal = MidiFile()
+
+trackfinal = MidiTrack()
+
+midfinal.tracks.append(trackfinal)
+
+notes = []
+
+for msg in midsource:
+
+    print(msg)
+
+notes.append(msg)
+
+#
+#
+# /Users/craigvear/PycharmProjects/RAMI/venv/bin/python /Users/craigvear/PycharmProjects/RAMI/tests/ignore.py
+# MIDI Type: 1
+# MetaMessage('track_name', name='Clipping', time=0)
+# MetaMessage('smpte_offset', frame_rate=30, hours=1, minutes=0, seconds=0, frames=0, sub_frames=0, time=0)
+# MetaMessage('time_signature', numerator=4, denominator=4, clocks_per_click=24, notated_32nd_notes_per_beat=8, time=0)
+# MetaMessage('set_tempo', tempo=500000, time=0)
+# MetaMessage('track_name', name='Track 1', time=0)

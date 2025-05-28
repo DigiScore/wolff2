89e11f132d748210e5d68d703ca45d8430298c60
Fabrizio Augusto Poltronieri <fabriziopoltronieri@gmail.com>
2025-05-28 18:16:44 +0200

    Add debug prints to test_randomize_modes
    Introduce print statements to display generated modes during tests. This helps in debugging by providing visibility into the modes generated in each test session.

--- tests/test_randomize_modes.py
+++ tests/test_randomize_modes.py
@@ -4,6 +4,9 @@ from modules.randomize_modes import generate_random_modes
 def test_randomize_modes():
     for _ in range(10):
         modes = generate_random_modes()
+        print("\nMODES FOR THIS SESSION:")
+        for i in modes:
+            print(f"\t{i}")
         ones = modes.count(1)
         zeros = modes.count(0)
         assert ones == zeros

5e33ab5295125eb3e6d4e18107fe31de1a1d1d70
Fabrizio Augusto Poltronieri <fabriziopoltronieri@gmail.com>
2025-05-29 23:19:06 +0200

    Refactor BITalino usage and update test scripts
    Replaced custom BITalino module with the official `bitalino` library in test scripts. Added a new test script `bitalino_for_test.py` for validating basic functionality and connection handling. Commented out unused `hivemind` and logging-related code in `bitalino_test.py` to simplify the test flow.

---
+++ tests/bitalino_for_test.py
@@ -0,0 +1,38 @@
+import bitalino
+import numpy
+import time
+
+from config import mac_address
+
+print(mac_address)
+
+device = bitalino.BITalino(mac_address)
+time.sleep(1)
+
+srate = 1000
+nframes = 100
+thresold = 5
+
+device.start(srate, [0])
+print("START")
+
+try:
+    while True:
+        data = device.read(nframes)
+
+        if numpy.mean(data[:, 1]) < 1 : break
+
+        EMG = data[:, -1]
+
+        envelope = numpy.abs(numpy.diff(EMG))
+
+        if envelope > thresold:
+            device.trigger([0, 1])
+        else:
+            device.trigger([0, 0])
+
+finally:
+    print("STOP")
+    device.trigger([0, 0])
+    device.stop()
+    device.close()
--- tests/bitalino_test.py
+++ tests/bitalino_test.py
@@ -1,13 +1,13 @@
 from time import time
 import config
 from modules.biodata_data_writer import BiodataDataWriter
-from modules.bitalino import BITalino
+import bitalino
 from nebula.hivemind import DataBorg
 
 
 # make new directory for this log e.g. ../data/20240908_123456
 master_path = f"../data/{time()}"
-hivemind = DataBorg()
+# hivemind = DataBorg()
 ###################
 # Bitalino
 ###################
@@ -19,7 +19,7 @@ BITALINO_ACQ_CHANNELS = config.channels
 eda_started = False
 while not eda_started:
     try:
-        eda = BITalino(BITALINO_MAC_ADDRESS)
+        eda = bitalino.BITalino(BITALINO_MAC_ADDRESS)
         eda_started = True
     except OSError:
         print("Unable to connect to Bitalino")
@@ -32,9 +32,9 @@ first_eda_data = "hiya"  # eda.read(1)[0]
 print(f"Data from BITalino = {first_eda_data}")
 
 
-hivemind.running = True
-
-
-test = BiodataDataWriter(master_path)
-test._test_running()
-test.main_loop()
+# hivemind.running = True
+#
+#
+# test = BiodataDataWriter(master_path)
+# test._test_running()
+# test.main_loop()

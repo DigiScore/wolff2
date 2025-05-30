3f2692687df28a4e2b917be73eaff2e609feb465
Fabrizio Augusto Poltronieri <fabriziopoltronieri@gmail.com>
2025-05-30 10:19:59 +0200

    Refactor BITalino handling with auto-reconnection logic.
    Introduced a `restart_bitalino` thread for automatic reconnection and enhanced error handling for BITalino sensor operations. Suppressed warnings to improve readability and ensure smoother execution. Updated imports and added safeguards for robustness.

--- nebula/nebula.py
+++ nebula/nebula.py
@@ -19,8 +19,10 @@ from scipy import signal
 from threading import Thread
 from time import sleep, time
 
+from sympy import false
+
 import config
-from modules.bitalino_module import BITalino
+from modules import bitalino_module
 
 # from modules.brainbit import BrainbitReader
 from modules.listener import Listener
@@ -70,6 +72,7 @@ class Nebula(Listener, AIFactoryRAMI):
             General tempo/ feel of Nebula's response (0.5 ~ moderate fast,
             1 ~ moderato, 2 ~ presto).
         """
+        warnings.simplefilter("ignore")
         print("Building engine server")
         Listener.__init__(self)
 
@@ -87,6 +90,8 @@ class Nebula(Listener, AIFactoryRAMI):
         # Work out master timing then collapse hivemind.running
         self.endtime = 0
 
+        self.bitalino_on = True
+
     def main_loop(self):
         """
         Starts the server / AI threads and gets the data rolling.
@@ -96,11 +101,46 @@ class Nebula(Listener, AIFactoryRAMI):
         t1 = Thread(target=self.make_data)
         t2 = Thread(target=self.snd_listen)
         t3 = Thread(target=self.human_input)
+        t4 = Thread(target=self.restart_bitalino)
 
         # Start them all
         t1.start()
         t2.start()
         t3.start()
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=DeprecationWarning)
+            t4.start()
+
+    def restart_bitalino(self):
+        warnings.simplefilter("ignore")
+        while True:
+            sleep(120)
+            self.bitalino_on = False
+            with warnings.catch_warnings():
+                try:
+                    warnings.filterwarnings("ignore", category=DeprecationWarning)
+                    self.eda.stop()
+                    self.eda.close()
+                except OSError:
+                    warnings.filterwarnings("ignore", category=DeprecationWarning)
+                    ...
+                bitalino_ok = false
+                while not bitalino_ok:
+                    try:
+                        warnings.filterwarnings("ignore", category=DeprecationWarning)
+                        self.eda = bitalino_module.BITalino(config.mac_address)
+                        bitalino_ok = True
+                    except OSError:
+                        warnings.filterwarnings("ignore", category=DeprecationWarning)
+                        ...
+                try:
+                    warnings.filterwarnings("ignore", category=DeprecationWarning)
+                    self.eda.start(config.baudrate, config.channels)
+                    self.bitalino_on = True
+                except OSError:
+                    warnings.filterwarnings("ignore", category=DeprecationWarning)
+                    ...
+
 
     def human_input(self):
         """
@@ -110,56 +150,68 @@ class Nebula(Listener, AIFactoryRAMI):
             if time() >= self.endtime:
                 break
             # Read data from bitalino
-            if self.BITALINO_CONNECTED:
-                # Get raw data from Bitalino
-                bitalino_raw = self.eda.read(1)[0]
+            if self.BITALINO_CONNECTED and self.bitalino_on:
+                try:
+                    warnings.filterwarnings("ignore", category=DeprecationWarning)
+                    # Get raw data from Bitalino
+                    bitalino_raw = self.eda.read(1)[0]
 
-                # write out the Bitalino data to the log
-                self.hivemind.bitalino_x = int(bitalino_raw[5])
+                    # write out the Bitalino data to the log
+                    self.hivemind.bitalino_x = int(bitalino_raw[5])
 
-                self.hivemind.bitalino_y = int(bitalino_raw[6])
+                    self.hivemind.bitalino_y = int(bitalino_raw[6])
 
-                self.hivemind.bitalino_z = int(bitalino_raw[7])
+                    self.hivemind.bitalino_z = int(bitalino_raw[7])
 
-                self.hivemind.bitalino_eda = int(bitalino_raw[8])
+                    self.hivemind.bitalino_eda = int(bitalino_raw[8])
 
-                # self.hivemind.bitalino_ecg = int(bitalino_raw[9])
-                #
-                # self.hivemind.bitalino_rsp = int(bitalino_raw[10])
+                    # self.hivemind.bitalino_ecg = int(bitalino_raw[9])
+                    #
+                    # self.hivemind.bitalino_rsp = int(bitalino_raw[10])
 
-                # extract eda for processing
-                eda_raw = [self.hivemind.bitalino_eda]  # [self.eda.read(1)[0][-1]]
+                    # extract eda for processing
+                    eda_raw = [self.hivemind.bitalino_eda]  # [self.eda.read(1)[0][-1]]
 
-                logging.debug(f"eda data raw = {eda_raw}")
+                    logging.debug(f"eda data raw = {eda_raw}")
 
-                # Update raw EDA buffer
-                eda_2d = np.array(eda_raw)[:, np.newaxis]
-                self.hivemind.eda_buffer_raw = np.append(
-                    self.hivemind.eda_buffer_raw, eda_2d, axis=1
-                )
-                self.hivemind.eda_buffer_raw = np.delete(
-                    self.hivemind.eda_buffer_raw, 0, axis=1
-                )
+                    # Update raw EDA buffer
+                    eda_2d = np.array(eda_raw)[:, np.newaxis]
+                    self.hivemind.eda_buffer_raw = np.append(
+                        self.hivemind.eda_buffer_raw, eda_2d, axis=1
+                    )
+                    self.hivemind.eda_buffer_raw = np.delete(
+                        self.hivemind.eda_buffer_raw, 0, axis=1
+                    )
 
-                # Detrend on the buffer time window
-                eda_detrend = signal.detrend(self.hivemind.eda_buffer_raw)
+                    # Detrend on the buffer time window
+                    eda_detrend = signal.detrend(self.hivemind.eda_buffer_raw)
 
-                # Get min and max from raw EDA buffer
-                eda_mins = np.min(eda_detrend, axis=1)
-                eda_maxs = np.max(eda_detrend, axis=1)
-                eda_mins = eda_mins - 0.05 * (eda_maxs - eda_mins)
+                    # Get min and max from raw EDA buffer
+                    eda_mins = np.min(eda_detrend, axis=1)
+                    eda_maxs = np.max(eda_detrend, axis=1)
+                    eda_mins = eda_mins - 0.05 * (eda_maxs - eda_mins)
 
-                # Rescale between 0 and 1
-                eda_norm = scaler(eda_detrend[:, -1], eda_mins, eda_maxs)
+                    # Rescale between 0 and 1
+                    eda_norm = scaler(eda_detrend[:, -1], eda_mins, eda_maxs)
 
-                # Update normalised EDA buffer
-                eda_2d = eda_norm[:, np.newaxis]
-                self.hivemind.eda_buffer = np.append(
-                    self.hivemind.eda_buffer, eda_2d, axis=1
-                )
-                self.hivemind.eda_buffer = np.delete(
-                    self.hivemind.eda_buffer, 0, axis=1
-                )
+                    # Update normalised EDA buffer
+                    eda_2d = eda_norm[:, np.newaxis]
+                    self.hivemind.eda_buffer = np.append(
+                        self.hivemind.eda_buffer, eda_2d, axis=1
+                    )
+                    self.hivemind.eda_buffer = np.delete(
+                        self.hivemind.eda_buffer, 0, axis=1
+                    )
+                except OSError:
+                    try:
+                        with warnings.catch_warnings():
+                            self.hivemind.eda_buffer = np.random.uniform(size=(1, 50))
+                            warnings.filterwarnings("ignore", category=DeprecationWarning)
+                            self.eda.stop()
+                            self.eda.close()
+                            self.eda.start(config.baudrate, config.channels)
+                    except OSError:
+                        ...
             else:
                 # Random data if no bitalino
                 self.hivemind.eda_buffer = np.random.uniform(size=(1, 50))
